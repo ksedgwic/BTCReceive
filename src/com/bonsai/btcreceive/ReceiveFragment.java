599c7442b3f371226779568435efdb55cdb08dd4
Ken Sedgwick <ken@bonsai.com>
2014-04-08 21:50:45 -0700

    Implemented intial receive fragment.

--- src/com/bonsai/btcreceive/ReceiveFragment.java
+++ src/com/bonsai/btcreceive/ReceiveFragment.java
@@ -15,16 +15,53 @@
 
 package com.bonsai.btcreceive;
 
+import java.math.BigInteger;
+import java.util.Hashtable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.bitcoin.core.Address;
+import com.google.bitcoin.uri.BitcoinURI;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ReceiveFragment extends Fragment {
+
+    private static Logger mLogger =
+        LoggerFactory.getLogger(ReceiveFragment.class);
+
+    private BaseWalletActivity mBase;
+
+	private final static QRCodeWriter sQRCodeWriter = new QRCodeWriter();
+
+    protected EditText mBTCAmountEditText;
+    protected EditText mFiatAmountEditText;
+    protected boolean mUserSetAmountFiat;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+        mBase = (BaseWalletActivity) getActivity();
 	}
 
 	@Override
@@ -39,6 +76,229 @@ public class ReceiveFragment extends Fragment {
                              Bundle savedInstanceState) {
 		View view =
             inflater.inflate(R.layout.receive_fragment, container, false);
+
+        // Start off presuming the user set the BTC amount.
+        mUserSetAmountFiat = false;
+
+        mBTCAmountEditText =
+            (EditText) view.findViewById(R.id.receive_btc_amount);
+        mFiatAmountEditText =
+            (EditText) view.findViewById(R.id.receive_fiat_amount);
+
+        mBTCAmountEditText.addTextChangedListener(mBTCAmountWatcher);
+        mFiatAmountEditText.addTextChangedListener(mFiatAmountWatcher);
+
+        mBTCAmountEditText.setOnEditorActionListener(checkForDone);
+        mFiatAmountEditText.setOnEditorActionListener(checkForDone);
+
+        ImageView iv = (ImageView) view.findViewById(R.id.receive_qr_view);
+        iv.setVisibility(View.GONE);
+
 		return view;
 	}
+
+	@Override
+	public void onResume() {
+        super.onResume();
+
+        BTCFmt btcfmt = mBase.getBTCFmt();
+
+        // Set these each time we resume in case we've visited the
+        // Settings and they've changed.
+        {
+            TextView tv =
+                (TextView) getActivity().findViewById(R.id.receive_btc_label);
+            tv.setText(btcfmt.unitStr());
+        }
+        mLogger.info("ReceiveFragment resumed");
+    }
+
+    // NOTE - This code implements a pair of "cross updating" fields.
+    // If the user changes the BTC amount the fiat field is constantly
+    // updated at the current mFiatPerBTC rate.  If the user changes
+    // the fiat field the BTC field is constantly updated at the
+    // current rate.
+
+    private final TextWatcher mBTCAmountWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence ss,
+                                          int start,
+                                          int count,
+                                          int after) {
+                // Note that the user changed the BTC last.
+                mUserSetAmountFiat = false;
+            }
+
+            @Override
+            public void onTextChanged(CharSequence ss,
+                                      int start,
+                                      int before,
+                                      int count) {}
+
+			@Override
+            public void afterTextChanged(Editable ss) {
+                updateAmountFields();
+            }
+
+        };
+
+    private final TextWatcher mFiatAmountWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence ss,
+                                          int start,
+                                          int count,
+                                          int after) {
+                mUserSetAmountFiat = true;
+            }
+
+            @Override
+            public void onTextChanged(CharSequence ss,
+                                      int start,
+                                      int before,
+                                      int count) {}
+
+			@Override
+            public void afterTextChanged(Editable ss) {
+                updateAmountFields();
+            }
+        };
+
+	protected void updateAmountFields() {
+        BTCFmt btcfmt = mBase.getBTCFmt();
+        double fiatPerBTC = mBase.fiatPerBTC();
+
+        // Which field did the user last edit?
+        if (mUserSetAmountFiat) {
+            // The user set the Fiat amount.
+            String ss = mFiatAmountEditText.getText().toString();
+
+            // Avoid recursion by removing the other fields listener.
+            mBTCAmountEditText.removeTextChangedListener
+                (mBTCAmountWatcher);
+
+            String bbs;
+            try {
+                double ff = parseNumberWorkaround(ss.toString());
+                long bb;
+                if (fiatPerBTC == 0.0) {
+                    bbs = "";
+                }
+                else {
+                    bb = btcfmt.btcAtRate(ff, fiatPerBTC);
+                    bbs = btcfmt.format(bb);
+                }
+            } catch (final NumberFormatException ex) {
+                bbs = "";
+            }
+            mBTCAmountEditText.setText(bbs, TextView.BufferType.EDITABLE);
+
+            // Restore the other fields listener.
+            mBTCAmountEditText.addTextChangedListener(mBTCAmountWatcher);
+        } else {
+            // The user set the BTC amount.
+            String ss = mBTCAmountEditText.getText().toString();
+
+            // Avoid recursion by removing the other fields listener.
+            mFiatAmountEditText.removeTextChangedListener
+                (mFiatAmountWatcher);
+
+            String ffs;
+            try {
+                long bb = btcfmt.parse(ss.toString());
+                double ff = btcfmt.fiatAtRate(bb, fiatPerBTC);
+                ffs = String.format("%.2f", ff);
+            } catch (final NumberFormatException ex) {
+                ffs = "";
+            }
+            mFiatAmountEditText.setText(ffs, TextView.BufferType.EDITABLE);
+
+            // Restore the other fields listener.
+            mFiatAmountEditText.addTextChangedListener(mFiatAmountWatcher);
+        }
+    }
+
+    private TextView.OnEditorActionListener
+        checkForDone = new TextView.OnEditorActionListener() {
+                public boolean onEditorAction(TextView view,
+                                              int actionId,
+                                              KeyEvent event) {
+                    mLogger.info("onEditorAction");
+                    showQRCode();
+                    return false;	// so it will take down the keyboard.
+                }
+            };
+
+    public void showQRCode() {
+        BTCFmt btcfmt = mBase.getBTCFmt();
+
+        Address addr = mBase.getWalletService().nextReceiveAddress();
+        String addrstr = addr.toString();
+
+        String ss = mBTCAmountEditText.getText().toString();
+        long bb = btcfmt.parse(ss.toString());
+        BigInteger amt = bb == 0 ? null : BigInteger.valueOf(bb);
+
+        String uri = BitcoinURI.convertToBitcoinURI(addrstr, amt, null, null);
+
+        mLogger.info("view address uri=" + uri);
+
+        final int size =
+            (int) (240 * getResources().getDisplayMetrics().density);
+
+        Bitmap bm = createBitmap(uri, size);
+        if (bm != null) {
+            ImageView iv = (ImageView) getActivity().findViewById(R.id.receive_qr_view);
+            iv.setImageBitmap(bm);
+        }
+
+        ImageView iv = (ImageView) getActivity().findViewById(R.id.receive_qr_view);
+        iv.setVisibility(View.VISIBLE);
+    }
+
+    private Bitmap createBitmap(String content, final int size) {
+        final Hashtable<EncodeHintType, Object> hints =
+            new Hashtable<EncodeHintType, Object>();
+        hints.put(EncodeHintType.MARGIN, 0);
+        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+        BitMatrix result;
+		try {
+			result = sQRCodeWriter.encode(content,
+                                          BarcodeFormat.QR_CODE,
+                                          size,
+                                          size,
+                                          hints);
+		} catch (WriterException ex) {
+            mLogger.warn("qr encoder failed: " + ex.toString());
+            return null;
+		}
+
+        final int width = result.getWidth();
+        final int height = result.getHeight();
+        final int[] pixels = new int[width * height];
+
+        for (int y = 0; y < height; y++)
+        {
+            final int offset = y * width;
+            for (int x = 0; x < width; x++)
+            {
+                pixels[offset + x] =
+                    result.get(x, y) ? Color.BLACK : Color.TRANSPARENT;
+            }
+        }
+
+        final Bitmap bitmap =
+            Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+
+        return bitmap;
+    }
+
+    private static double parseNumberWorkaround(String numstr)
+        throws NumberFormatException {
+        // Some countries use comma as the decimal separator.
+        // Android's numberDecimal EditText fields don't handle this
+        // correctly (https://code.google.com/p/android/issues/detail?id=2626).
+        // As a workaround we substitute ',' -> '.' manually ...
+        return Double.parseDouble(numstr.toString().replace(',', '.'));
+    }
 }
